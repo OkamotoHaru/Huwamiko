dfc4599d08e0d9db88d4fb3e74c8494c0be800d6
OkamotoHaru <truevr.lisbeth@gmail.com>
2019-01-04 17:11:11 +0900

    ・結果画面　アイテム表示部分　作成途中

--- front/src/js/calculator.js
+++ front/src/js/calculator.js
@@ -31,65 +31,105 @@ function setSearchInput(){
 
 //--------------------------------------------------
 
-/// 検索結果テーブル作成
 function setSearchTable(){
 	//データ設定
 	var inputText = "羅刹剣"
-	//中間アイテム・素材アイテムリスト設定
-	let nameNo = 0
-	var input_item = null
-	for(var i=0; i<item_data.length; i++){
-		if ( item_data[i][nameNo] == inputText ){
-			input_item = item_data[i]
-			console.log(input_item)
+	var inputItemData = null
+	for (var i=0; i<item_data.length; i++){
+		if (inputText == item_data[i][0]){
+			inputItemData = item_data[i]
 			break
 		}
 	}
-	var matNo = 4
-	var sub_items = []
+	//テーブル設定
+	let matNo = 4
+	for (var i=0; i<inputItemData[matNo].length; i++){
+		setListTable(inputItemData[matNo][i][0], inputItemData[matNo][i][1])
+	}
+}
+
+function setListTable(name, num){
+
 	for (var i=0; i<item_data.length; i++){
-		//
-		for (var j=0; j<input_item[matNo].length; j++){
-			if (item_data[i][nameNo] == input_item[matNo][j][0]){
-				sub_items.push( item_data[i] )
+		let nameNo = 0
+		if (item_data[i][nameNo] == name){
+			let jobNo = 1
+			if (item_data[i][jobNo] == "" || item_data[i][jobNo] == "園芸師" || item_data[i][jobNo] == "採掘師"){
+				var matTable = document.getElementById("mat_table")
+				//素材テーブルに追加
+				var tr = document.createElement("tr")
+				matTable.appendChild(tr)
+				//名前
+				var td_name = document.createElement("td")
+				td_name.textContent = item_data[i][nameNo]
+				tr.appendChild(td_name)
+				//個数
+				var td_num = document.createElement("td")
+				td_num.textContent = num
+				tr.appendChild(td_num)
+				//所持済
+				var td_have = document.createElement("td")
+				tr.appendChild(td_have)
+				var select = document.createElement("select")
+				select.name = "have"
+				td_have.appendChild(select)
+				var options_text = ["プレイヤー","Juliane","Yuigahama","チョコボかばん","ミラージュドレッサー","カンパニーチェスト"]
+				for (var j=0; j<options_text.length; j++){
+					var option = document.createElement("option")
+					option.textContent = options_text[j]
+					select.appendChild( option )
+				}
+				//ジョブ
+				var td_job = document.createElement("td")
+				td_job.textContent = item_data[i][jobNo]
+				tr.appendChild(td_job)
+				//テレポ
+				var td_telep = document.createElement("td")
+				td_telep.textContent = "telep"
+				tr.appendChild(td_telep)
+				//座標
+				var td_pos = document.createElement("td")
+				td_pos.textContent = "X: Y:"
+				tr.appendChild(td_pos)
+			}
+			else{
+				var itemTable = document.getElementById("item_table")
+				//素材テーブルに追加
+				var tr = document.createElement("tr")
+				itemTable.appendChild(tr)
+				//名前
+				var td_name = document.createElement("td")
+				td_name.textContent = item_data[i][nameNo]
+				tr.appendChild(td_name)
+				//個数
+				var td_num = document.createElement("td")
+				td_num.textContent = num
+				tr.appendChild(td_num)
+				//所持済
+				var td_have = document.createElement("td")
+				tr.appendChild(td_have)
+				var select = document.createElement("select")
+				select.name = "have"
+				td_have.appendChild(select)
+				var options_text = ["プレイヤー","Juliane","Yuigahama","チョコボかばん","ミラージュドレッサー","カンパニーチェスト"]
+				for (var j=0; j<options_text.length; j++){
+					var option = document.createElement("option")
+					option.textContent = options_text[j]
+					select.appendChild( option )
+				}
+				//ジョブ
+				var td_job = document.createElement("td")
+				td_job.textContent = item_data[i][jobNo]
+				tr.appendChild(td_job)
+				//テレポ
+				var td_telep = document.createElement("td")
+				td_telep.textContent = "telep"
+				tr.appendChild(td_telep)
+
+				for(var j=0; j<item_data[i][4].length; j++){
+					setListTable( item_data[i][4][j][0], item_data[i][4][j][1])
+				}
 			}
 		}
-	}
-	console.log(sub_items)
-	//テーブル設定
-	var itemTable = document.getElementById("item_table")
-	var matTable = document.getElementById("mat_table")
-	for (var i=0; i<sub_items.length; i++){
-		//ジョブがない、採掘師、園芸師なら素材アイテム
-		var setTable = null
-		if ( sub_items[i][1] == "" || sub_items[i][1] == "採掘師" || sub_items[i][1] == "園芸師" ){
-			setTable = matTable
-		}
-		else{
-			setTable = itemTable
-		}
-		//行
-		var tr = document.createElement("tr")
-		setTable.appendChild(tr)
-		//名前
-		var td_name = document.createElement("td")
-		td_name.textContent = sub_items[i][0]
-		tr.appendChild(td_name)
-		//ジョブ
-		var td_job = document.createElement("td")
-		td_job.textContent = sub_items[i][1]
-		tr.appendChild(td_job)
-		//習得方法
-		var td_learn = document.createElement("td")
-		td_learn.textContent = sub_items[i][2]
-		tr.appendChild(td_learn)
-		//難易度
-		var td_diff = document.createElement("td")
-		td_diff.textContent = sub_items[i][3]
-		tr.appendChild(td_diff)
-		//テレポ
-		var td_telep = document.createElement("td")
-		td_telep.textContent = sub_items[i][5]
-		tr.appendChild(td_telep)
 	}
 }
